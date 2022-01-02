fae798855dc5ad92334439f0ecfef76244b02f29
Douglas Silva <douglassimoesds@gmail.com>
2022-01-02 12:24:06 +0100

    Add respective views and urls to projects app

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret key used in production secret! 
+# from django.core.management.utils import get_random_secret_key  
+# get_random_secret_key()
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
--- projects/views.py
+++ projects/views.py
@@ -7,3 +7,8 @@ def all_projects(request):
     # fetch all the projects in the database
     projects = Project.objects.all()
     return render(request, "projects/index.html", {"projects":projects})
+
+# pk is been passed by parameter/argument from urls
+def project_details(request, pk):
+    project = Project.objects.get(pk=pk)
+    return render( request, "projects/detail.html", {"project":project})
